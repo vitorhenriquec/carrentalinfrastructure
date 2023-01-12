2afa4b269341c54c771f08036f71193573ec31ba
Vitor Henrique <vitorhenrique908@gmail.com>
2023-01-12 09:25:43 -0300

    Add entity car e update database properties

---
+++ carrental/src/main/java/com/github/vitorhenriquec/carrental/model/Car.java
@@ -0,0 +1,28 @@
+package com.github.vitorhenriquec.carrental.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "car")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @Column(name = "model", nullable = false)
+    private String model;
+
+    @Column(name = "brand", nullable = false)
+    private Boolean available;
+}
