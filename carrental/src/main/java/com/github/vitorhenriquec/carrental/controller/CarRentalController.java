adb97163082a68b72eeef3894455d21b57c8d2ed
Vitor Henrique <vitorhenrique908@gmail.com>
2023-01-12 13:49:44 -0300

    Add swagger documentation to the add car flow

--- carrental/src/main/java/com/github/vitorhenriquec/carrental/controller/CarRentalController.java
+++ carrental/src/main/java/com/github/vitorhenriquec/carrental/controller/CarRentalController.java
@@ -2,29 +2,20 @@ package com.github.vitorhenriquec.carrental.controller;
 
 import com.github.vitorhenriquec.carrental.request.CarSaveRequest;
 import com.github.vitorhenriquec.carrental.response.CarSaveResponse;
-import com.github.vitorhenriquec.carrental.service.CarRentalService;
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/v1/car")
-@AllArgsConstructor
-@Log4j2
-public class CarRentalController {
+@Tag(
+        name= "Car Rental",
+        description= "Car rental service"
+)
+public interface CarRentalController {
 
-    private final CarRentalService carRentalService;
-
-    @PostMapping(consumes="application/json", produces = "application/json")
-    public ResponseEntity<CarSaveResponse> saveCar(
-            @RequestBody CarSaveRequest carSaveRequest
-    ) {
-        log.info("method={}", "saveCar");
-        return ResponseEntity.ok(carRentalService.saveCard(carSaveRequest));
-    }
+    @Operation(description = "Save a new car")
+    ResponseEntity<CarSaveResponse> saveCar(
+            @Parameter(name="body", required = true, description = "Car data to be saved") CarSaveRequest carSaveRequest
+    );
 
 }
---
+++ carrental/src/main/java/com/github/vitorhenriquec/carrental/controller/CarRentalControllerImpl.java
@@ -0,0 +1,32 @@
+package com.github.vitorhenriquec.carrental.controller;
+
+import com.github.vitorhenriquec.carrental.request.CarSaveRequest;
+import com.github.vitorhenriquec.carrental.response.CarSaveResponse;
+import com.github.vitorhenriquec.carrental.service.CarRentalService;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/car")
+@AllArgsConstructor
+@Log4j2
+public class CarRentalControllerImpl implements CarRentalController {
+
+    private final CarRentalService carRentalService;
+
+
+    @Override
+    @PostMapping(consumes="application/json", produces = "application/json")
+    public ResponseEntity<CarSaveResponse> saveCar(
+            @RequestBody CarSaveRequest carSaveRequest
+    ) {
+        log.info("method={}", "saveCar");
+        return ResponseEntity.ok(carRentalService.saveCard(carSaveRequest));
+    }
+
+}
