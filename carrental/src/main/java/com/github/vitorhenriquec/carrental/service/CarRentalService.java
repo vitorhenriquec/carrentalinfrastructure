34a1900cbcb636156c92b8ea130c5b39f8ee191c
Vitor Henrique <vitorhenrique908@gmail.com>
2023-01-12 21:57:31 -0300

    Add search flow for diferent types of columns

--- carrental/src/main/java/com/github/vitorhenriquec/carrental/service/CarRentalService.java
+++ carrental/src/main/java/com/github/vitorhenriquec/carrental/service/CarRentalService.java
@@ -1,9 +1,12 @@
 package com.github.vitorhenriquec.carrental.service;
 
 import com.github.vitorhenriquec.carrental.exception.CarNotFoundException;
+import com.github.vitorhenriquec.carrental.model.Car;
 import com.github.vitorhenriquec.carrental.request.CarSaveRequest;
 import com.github.vitorhenriquec.carrental.request.CarUpdateRequest;
 import com.github.vitorhenriquec.carrental.response.CarSaveResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CarRentalService {
 
